7f0db443c9150e7328508687752833827f492afd
lzgdjl <lzgdjl@126.com>
2017-02-14 23:10:07 +0800

    add Channel.h && EventLoop.h

---
+++ Channel.h
@@ -0,0 +1,54 @@
+#ifndef __YOUNG_NET_CHANNEL_H__
+#define __YOUNG_NET_CHANNEL_H__
+
+#include <functional>
+
+namespace YoungNet
+{
+
+	class Channel
+	{
+	public:
+		Channel(int fd, EventLoop*);
+		Channel(const Channel&) = delete;
+		Channel(Channel&&) = delete;
+		Channel& operator=(const Channel&) = delete;
+		Channel& operator=(Channel&&) = delete;
+		
+		void EnableRead();
+		void EnableWrite();
+		void EnableAll();
+		void DisableAll();
+		void RemoveListen();
+		
+
+		using HandleCallback = std::functional<int()>;
+
+		void SetReadCallback(const HandleCallback&);
+		void SetReadCallback(HandleCallback&&);
+		
+		void SetWriteCallback(const HandleCallback&);
+		void SetWriteCallback(HandleCallback&&);
+
+		void SetCloseCallback(const HandleCallback&);
+		void SetCloseCallback(HandleCallback&&);
+	
+		void HandleRead();
+		void HandleWrite();
+		void HandleClose();
+		void HandleError();
+	
+	private:
+		int listenFd_;
+		EventLoop loop_;
+		int listenEvent_;
+		int returnEvent_;
+
+		HandleCallback readCallback_;
+		HandleCallback writeCallback_;
+		HandleCallback closeCallback_;
+	};
+} // namespace YoungNet
+
+#endif // __YOUNG_NET_CHANNEL_H__
+
---
+++ EventLoop.h
@@ -0,0 +1,38 @@
+#ifndef __YOUNG_NET_EVENTLOOP_H__
+#define __YOUNG_NET_EVENTLOOP_H__
+
+#include <vector>
+
+struct epoll_event;
+
+namespace YoungNet
+{
+
+	class Channel;
+
+	class EventLoop
+	{
+	public:
+		EventLoop();
+		~EventLoop() noexcept;
+
+		EventLoop(const EventLoop&) = delete;
+		EventLoop(EventLoop&&)= delete;
+		EventLoop& operator=(const EventLoop&) = delete;
+		EventLoop& operator=(EventLoop&&) = delete;
+		
+		void UpdateChannel(const Channel*);
+		void RemoveChannel(const Channel*);
+		void StartLoop();
+
+	private:
+		std::vector<Channel*> GetReadyChannel();
+
+		int efd_;
+		std::vector<struct epoll_event> readyEvent_;
+		std::set<Channel*> channelContainer_;				
+	};
+} // namespace YoungNet
+
+#endif // __YOUNG_NET_EVENTLOOP_H__
+
